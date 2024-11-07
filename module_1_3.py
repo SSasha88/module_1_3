137b70ab88e317da5d34db77211542961fd691ab
SSasha88 <d1rus1853@gmail.com>
2024-11-07 09:39:41 +0300

    1

--- module_1_3.py
+++ module_1_3.py
@@ -3,4 +3,4 @@ print("Name: " + Name, type(Name))
 Age = 36
 print("Age: " + str(Age), type(Age))
 print("New Age: " + str(int(Age+1)), type(Age))
-print('Is_Student: ')
+print('Is_Student: ', Age<int(Age+1)), print(type(Age<int(Age+1)))
