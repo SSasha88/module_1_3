5d02c6c0735a6d8b6f2676459e099c35a0f29c89
SSasha88 <d1rus1853@gmail.com>
2024-11-08 12:38:39 +0300

    1

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+Home_work = 12
+Time_spent = 1.5
+Course_name = 'Python'
+time_1_work = Time_spent/Home_work
+print('Kурс:', Course_name,', всего задач:', Home_work, ', затрачено часов:', Time_spent, ', среднее время выполнения:', time_1_work,'часа' )
